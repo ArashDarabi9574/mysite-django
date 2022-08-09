95be147f1da5c5fb78cd30f1d37a33a7626ddfb2
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-09 11:57:27 +0430

    general templates add

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.http import HttpResponse
 
 def index_view(response):
-     return render(response, 'index.html')
+     return render(response, 'website/index.html')
 
 def about_view(response):
-     return render(response, 'about.html')
+     return render(response, 'website/about.html')
 
 def contact_view(response):
-     return render(response, 'contact.html')
+     return render(response, 'website/contact.html')
