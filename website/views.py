00c0deeb8d9953c4f6ba0e349ba71e37764f5c77
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-10 13:59:01 +0430

    dynamic urls added

--- website/views.py
+++ website/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponse
 
+
 def index_view(response):
-     return render(response, 'website/index.html')
+    return render(response, 'website/index.html')
+
 
 def about_view(response):
-     return render(response, 'website/about.html')
+    return render(response, 'website/about.html')
+
 
 def contact_view(response):
-     return render(response, 'website/contact.html')
+    return render(response, 'website/contact.html')
+
+
+def elemntry_view(response):
+    return render(response, 'website/elements.html')
