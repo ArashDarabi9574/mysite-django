03df90edd6c88f27f5ef3aee8c14a75d9c61f960
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-13 18:01:54 +0430

    update_data base again

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 class Post(models.Model):
-     title = models.CharField(max_length=255, blank=True)
-     content = models.TextField()
+    # image
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    content_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
