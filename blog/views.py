fe5703ce4be6b13c18d5a0dfd05a6dc93adee807
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-14 19:07:50 +0430

    single models db updated

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
 def blog_view(response):
     posts = Post.objects.filter(published_at__lte=datetime.datetime.now())
     context = {'posts': posts}
-    
     return render(response, 'blog/blog-home.html', context)
 
 
-def blog_single_view(response):
-    return render(response, 'blog/blog-single.html')
+def blog_single_view(response, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post': post}
+    return render(response, 'blog/blog-single.html', context)
