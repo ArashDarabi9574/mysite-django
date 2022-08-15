6266bbf0b4ac328618c3a658cf17c275c4ad9e17
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-15 19:58:27 +0430

    views back

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,13 @@ from blog.models import Post
 
 
 def blog_view(response):
-    posts = Post.objects.filter(published_at__lte=datetime.datetime.now())
+    posts = Post.objects.filter(status=1)
     context = {'posts': posts}
     return render(response, 'blog/blog-home.html', context)
 
 
 def blog_single_view(response, pid):
-    posts = Post.objects.filter(published_at__lte=datetime.datetime.now())
+    posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid, status=1)
     context = {'post': post}
     return render(response, 'blog/blog-single.html', context)
