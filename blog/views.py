c95b627000090d3e2027840ad27554d676d87b05
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-16 13:29:45 +0430

    next prev added

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,12 @@ def blog_view(response):
 
 
 def blog_single_view(response, pid):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_at__lte=datetime.datetime.now())
     post = get_object_or_404(posts, pk=pid, status=1)
     post.content_view +=1
     post.save()
-
-    context = {'post': post}
+    nextpost = Post.objects.filter(id__gt=post.pk ).order_by('pk').first()
+    prevpost = Post.objects.filter(id__lt=post.pk).order_by('pk').last()
+    context = {'post': post, 'nextpost': nextpost, 'prevpost': prevpost}
 
     return render(response, 'blog/blog-single.html', context)
