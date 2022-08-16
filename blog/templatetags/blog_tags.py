19aa1e83deee91a078f65d81414543550021427c
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-16 17:12:47 +0430

    pages template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.simple_tag(name="totalposts")
 def totalposts():
-     posts = Post.objects.filter(status=1)
+     posts = Post.objects.filter(status=1).count()
      return posts
