ea8a6017cf4bf0507bccdb4b611d063950a99399
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-16 17:37:47 +0430

    template popular added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 def totalposts():
      posts = Post.objects.filter(status=1).count()
      return posts
+
+@register.inclusion_tag('blog/blog-popular-post.html')
+def latestpost():
+     posts = Post.objects.filter(status=1).order_by('published_at')[:4]
+     return {'posts': posts}
