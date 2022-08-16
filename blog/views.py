534de57ea9d7ee7ea57992ff9eb20f8e10f221fa
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-16 12:19:38 +0430

    fix count view

--- blog/views.py
+++ blog/views.py
@@ -12,5 +12,9 @@ def blog_view(response):
 def blog_single_view(response, pid):
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid, status=1)
+    post.content_view +=1
+    post.save()
+
     context = {'post': post}
+
     return render(response, 'blog/blog-single.html', context)
