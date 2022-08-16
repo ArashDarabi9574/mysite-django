d2b27c13bacd5c58613b320ba41bacfae151ce0e
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-16 15:27:34 +0430

    simple template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,9 @@
+from os import stat
 from django import template
-
+from blog.models import Post
 register = template.Library()
+
+@register.simple_tag(name="totalposts")
+def totalposts():
+     posts = Post.objects.filter(status=1)
+     return posts
