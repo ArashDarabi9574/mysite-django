5c4335fe66104c85c4cc11a59c038e5c08c7157e
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-17 09:55:30 +0430

    main.css and tags are added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,14 +1,28 @@
 from os import stat
+from unicodedata import category
 from django import template
 from blog.models import Post
+from blog.models import Category
 register = template.Library()
+
 
 @register.simple_tag(name="totalposts")
 def totalposts():
-     posts = Post.objects.filter(status=1).count()
-     return posts
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
 
 @register.inclusion_tag('blog/blog-popular-post.html')
 def latestpost():
-     posts = Post.objects.filter(status=1).order_by('published_at')[:4]
-     return {'posts': posts}
+    posts = Post.objects.filter(status=1).order_by('published_at')[:4]
+    return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    category_dict = {}
+    for name in categories:
+        category_dict[name] = posts.filter(category=name).count()
+    return {'categories': category_dict}
