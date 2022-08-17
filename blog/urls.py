539d5ea6aebf24d4122603d6c023868df45819a3
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-17 13:24:09 +0430

    author user added

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single_view, name='single'),
     path('category/<str:cat_name>', blog_view, name='category'),
+    path('author/<str:author_username>', blog_view, name='author'),
 ]
