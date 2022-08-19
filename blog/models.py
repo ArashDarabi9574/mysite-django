1ee3d36c59c61c519d7b23aa2fc48fbd665dd8f8
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-19 12:39:30 +0430

    sitemaps.xml

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,14 @@ from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+ 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
@@ -15,12 +18,14 @@ class Post(models.Model):
     content = models.TextField()
     # tag
     category = models.ManyToManyField(Category)
-    content_view=models.IntegerField(default=0)
+    content_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_at = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return "{} - {}" .format(self.title, self.id)
+
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid':self.id})
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
     'django.contrib.humanize',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog'
 ]
 
