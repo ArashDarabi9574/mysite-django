2b9227a399274a475c815986a80c2385bd773cfc
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-21 11:50:55 +0430

    signin added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ app_name = 'accounts'
 urlpatterns = [
      path('login',login_view, name='login'),
      #login
-     # path('logout',views.logout_view, name='logout'),
-     # #logout
+     path('logout',logout_view, name='logout'),
+     #logout
      path('signup',signup_view, name='signup'),
      #signup
 
