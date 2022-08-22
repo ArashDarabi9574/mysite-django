75799cb58c5130dc410328005d44695fac55bbb3
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-08-22 13:57:35 +0430

    aaaa

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,3 +172,10 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# google disable security less options!
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
